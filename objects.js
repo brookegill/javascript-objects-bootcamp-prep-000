b3b06205b7a73e8863aeea49bc2e336d18d83c61
Brooke Gill <brooke.gill25@gmail.com>
2018-11-09 00:58:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
+<<<<<<< HEAD
   playlist['Slowdive'] = "Alison"
   playlist['My Bloody Valentine'] = "Sometimes"
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
@@ -12,4 +13,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
 function removeFromPlaylist(playlist, artistName) {
   delete playlist.Slowdive
   return playlist
+=======
+  return Object.assign({}, playlist, { [Alison]: "Slowdive", [My Bloody Valentine]: "Sometimes", [Phil Ochs]: "Here's to the State of Mississippi"})
+>>>>>>> 8811d2d13af0bc886a912c5c4ae92051a1ab556c
 };
